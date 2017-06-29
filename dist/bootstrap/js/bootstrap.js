c0325a5e8471aa3027d6c3acd457f31d76b6ecea
Vladimir Alexandrov <dev.alexandrov@gmail.com>
2017-06-29 16:54:16 +0300

    mobile

--- dist/bootstrap/js/bootstrap.js
+++ dist/bootstrap/js/bootstrap.js
@@ -334,7 +334,7 @@ if (typeof jQuery === 'undefined') {
   Carousel.TRANSITION_DURATION = 600
 
   Carousel.DEFAULTS = {
-    interval: false,
+    interval: 5000,
     pause: 'hover',
     wrap: true,
     keyboard: true
